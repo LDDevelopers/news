360706462e03b9ed599dcc549dd819c2efe99355
LDDeveloper <112258743+LDDevelopers@users.noreply.github.com>
2023-04-01 18:33:05 +0530

    added: card html and css

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,13 +3,55 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <div className="bg-yellow-500">
-        <div>
-          <h2>Disha Epaper</h2>
-          <nav>
-            <ul>
-              <li>
-                <Link></Link>
+      <div className="">
+        <div className="wt-80 m-auto">
+          <div className="flex flex-col justify-center items-center gap-5 bg-yellow-500 py-8">
+            <figure className="">
+              <img
+                src="https://marketing.readwhere.com/epaper-logo/disha-daily-logo.png"
+                alt=""
+                className="max-w-2xl"
+              />
+            </figure>
+
+            <div className="">
+              <ul className="flex gap-10 text-2xl">
+                <li>
+                  <i className="fa-brands fa-facebook"></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-whatsapp"></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-instagram"></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-twitter"></i>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <nav className="py-5 border-2 border-solid">
+            <ul className="flex  ">
+              <li className="flex-1">
+                <Link>Home</Link>
+              </li>
+              <li className="flex-1">
+                <Link>About</Link>
+              </li>
+              <li className="flex-1">
+                <Link className="flex items-center justify-center gap-1">
+                  <span>State</span>
+                  <span>
+                    <i className="fa-solid fa-angle-down"></i>
+                  </span>
+                </Link>
+              </li>
+              <li className="flex-1">
+                <Link>Contact</Link>
+              </li>
+              <li className="flex-1">
+                <Link>Upload</Link>
               </li>
             </ul>
           </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,90 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="bg-white m-x-6">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className=" mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-1">
+            <div className="group relative">
+              <img
+                className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+                alt=""
+                src="assests/bottle.jpg"
+              />
+              <div className="mt-4 w-full flex justify-center text-center">
+                <div className="text-center">
+                  <h3 className="text-sm text-gray-700">
+                    <Link to="text-center">TS-Main</Link>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="group relative">
+              <img
+                className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+                alt=""
+                src="assests/bottle.jpg"
+              />
+              <div className="mt-4 flex justify-center">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <Link to="">
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-0"
+                      ></span>
+                      TS-Main
+                    </Link>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="group relative">
+              <img
+                className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+                alt=""
+                src="assests/bottle.jpg"
+              />
+              <div className="mt-4 flex justify-center">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <Link to="">
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-0"
+                      ></span>
+                      TS-Main
+                    </Link>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="group relative">
+              <img
+                className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+                alt=""
+                src="assests/bottle.jpg"
+              />
+              <div className="mt-4 flex justify-center">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <Link to="">
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-0"
+                      ></span>
+                      TS-Main
+                    </Link>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
